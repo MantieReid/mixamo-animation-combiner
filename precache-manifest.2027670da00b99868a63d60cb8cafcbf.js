4d9a9d199afe0322bd0d5107157c6dda05f4b4e1
theprogboy <mdrezwanferdous@gmail.com>
2020-08-30 11:33:48 +0200

    Updates

---
+++ precache-manifest.2027670da00b99868a63d60cb8cafcbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446d0708aae3ee223974fb7def409cde",
+    "url": "/mixamocomb/index.html"
+  },
+  {
+    "revision": "6d2ba8992924cb8c5e12",
+    "url": "/mixamocomb/static/css/2.9dff4bae.chunk.css"
+  },
+  {
+    "revision": "aab0e98546f1af5978b1",
+    "url": "/mixamocomb/static/css/main.30c9419f.chunk.css"
+  },
+  {
+    "revision": "6d2ba8992924cb8c5e12",
+    "url": "/mixamocomb/static/js/2.9d551b8f.chunk.js"
+  },
+  {
+    "revision": "927833c246ca48f9c0c996dc43814e11",
+    "url": "/mixamocomb/static/js/2.9d551b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab0e98546f1af5978b1",
+    "url": "/mixamocomb/static/js/main.550c4f17.chunk.js"
+  },
+  {
+    "revision": "c23df17536adb349a4ac",
+    "url": "/mixamocomb/static/js/runtime-main.1dd1d1d7.js"
+  }
+]);
